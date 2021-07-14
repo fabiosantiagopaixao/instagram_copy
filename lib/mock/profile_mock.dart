708525ec120b5bc8446a0bd66276f2adfc313094
Fábio S. Paixão <fabio.santiago99@gmail.com>
2021-07-14 16:13:45 -0400

    refactor: remove old code

--- lib/mock/profile_mock.dart
+++ lib/mock/profile_mock.dart
@@ -9,11 +9,11 @@ var profileTest = new Profile(
     storys: storysTest);
 
 var storysTest = [
-  Story(name: "Jaqueline Santiago", profileImage: "assets/profile1.jpg"),
-  Story(name: "Maria Santiago", profileImage: "assets/profile2.jpg"),
-  Story(name: "Carla Assunção", profileImage: "assets/profile3.jpg"),
-  Story(name: "Adriano Souza", profileImage: "assets/profile4.jpg"),
-  Story(name: "Sandy Oliveira", profileImage: "assets/profile5.jpg"),
-  Story(name: "Jaqueline Santiago", profileImage: "assets/profile6.jpg"),
-  Story(name: "Jessica Moraes", profileImage: "assets/profile7.jpg")
+  new Story(name: "Jaqueline Santiago", profileImage: "assets/profile1.jpg"),
+  new Story(name: "Maria Santiago", profileImage: "assets/profile2.jpg"),
+  new Story(name: "Carla Assunção", profileImage: "assets/profile3.jpg"),
+  new Story(name: "Adriano Souza", profileImage: "assets/profile4.jpg"),
+  new Story(name: "Sandy Oliveira", profileImage: "assets/profile5.jpg"),
+  new Story(name: "Jaqueline Santiago", profileImage: "assets/profile6.jpg"),
+  new Story(name: "Jessica Moraes", profileImage: "assets/profile7.jpg")
 ];
