55bf3a9f389babf3075ed4152a0a40e2d3cea8e8
Fábio S. Paixão <fabio.santiago99@gmail.com>
2021-07-17 16:38:23 -0400

    feat: init screen reels

---
+++ lib/screens/tabs/reels_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ReelsScreen extends StatelessWidget {
+  const ReelsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reels"),
+      ),
+    );
+  }
+}
